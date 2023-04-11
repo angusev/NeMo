fdce20a95d91f53e2f9ed8ef27ed416ee02ee3d6
Andrey Gusev <gusevgaa@gmail.com>
2023-04-11 13:56:06 +0000

    add my training script

---
+++ my_train_hifigan.sh
@@ -0,0 +1,16 @@
+cp -r /home/andrey/NeMo_notebooks/*_mels ./
+cp /home/andrey/NeMo_notebooks/*_ft.json ./
+
+python examples/tts/hifigan_finetune.py \
+    --config-name=hifigan.yaml \
+    model.train_ds.dataloader_params.batch_size=32 \
+    model.max_steps=1000000 \
+    model.optim.lr=0.00001 \
+    ~model.optim.sched \
+    train_dataset=./hifigan_train_ft.json \
+    validation_datasets=./hifigan_val_ft.json \
+    exp_manager.exp_dir=hifigan_ft \
+    +init_from_pretrained_model=tts_hifigan \
+    trainer.check_val_every_n_epoch=100 \
+    model/train_ds=train_ds_finetune \
+    model/validation_ds=val_ds_finetune
